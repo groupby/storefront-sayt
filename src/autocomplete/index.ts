8312ea9b607a3da822fbf3ce0c01ca642a955ea1
Ben Teichman <ben.l.teichman@gmail.com>
2017-06-17 18:17:42 -0400

    support all activation methods

--- src/autocomplete/index.ts
+++ src/autocomplete/index.ts
@@ -4,26 +4,79 @@ import { alias, tag, Events, Store, Tag } from '@storefront/core';
 @tag('gb-sayt-autocomplete', require('./index.html'))
 class Autocomplete {
 
-  state: Autocomplete.State = {
-    category: this.flux.store.getState().data.autocomplete.category.field,
-    categoryValues: this.flux.store.getState().data.autocomplete.category.values,
-    suggestions: this.flux.store.getState().data.autocomplete.suggestions,
-    navigations: this.flux.store.getState().data.autocomplete.navigations
-  };
+  constructor() {
+    const {
+      suggestions,
+      navigations,
+      category: { field: category, values: categoryValues }
+    } = this.flux.store.getState().data.autocomplete;
+    this.state = { suggestions, navigations, category, categoryValues, selected: -1 };
+  }
 
   init() {
-    // TODO register with autocomplete service (sayt service?) so it only runs if tags exist
+    this.services.autocomplete.registerAutocomplete(this);
     this.flux.on(Events.AUTOCOMPLETE_SUGGESTIONS_UPDATED, this.updateSuggestions);
+    this.flux.on('sayt:activate_next', this.activateNext);
+    this.flux.on('sayt:activate_previous', this.activatePrevious);
+    this.flux.on('sayt:select_active', this.selectActive);
+  }
+
+  activationTargets() {
+    return this.root.querySelectorAll('.gb-autocomplete-target');
+  }
+
+  activateNext = () => {
+    const targets = this.activationTargets();
+    let selected = this.state.selected;
+    if (selected < targets.length - 1) {
+      if (this.isActive()) {
+        this.setActivation(targets, selected, false);
+      }
+      this.setActivation(targets, ++selected, true);
+    }
+  }
+
+  activatePrevious = () => {
+    const targets = this.activationTargets();
+    if (this.isActive()) {
+      let selected = this.state.selected;
+      this.setActivation(targets, selected, false);
+      if (--selected !== -1) {
+        this.setActivation(targets, selected, true);
+      }
+    }
+  }
+
+  selectActive = () => {
+    if (this.isActive()) {
+      this.activationTargets()[this.state.selected].querySelector('a').click();
+      this.set({ selected: -1 });
+    }
   }
 
   updateSuggestions = ({ suggestions, navigations, category: { values: categoryValues } }: Store.Autocomplete) => {
-    this.set({ suggestions, navigations, categoryValues });
+    if (this.isActive()) {
+      this.setActivation(this.activationTargets(), this.state.selected, false);
+    }
+    this.set({ suggestions, navigations, categoryValues, selected: -1 });
+  }
+
+  setActivation(targets: NodeListOf<Element>, index: number, activate: boolean) {
+    targets[index].classList[activate ? 'add' : 'remove']('gb-active');
+    if (activate) {
+      this.state.selected = index;
+    }
+  }
+
+  isActive() {
+    return this.state.selected !== -1;
   }
 }
 
 interface Autocomplete extends Tag<any, Autocomplete.State> { }
 namespace Autocomplete {
   export interface State {
+    selected: number;
     category: string;
     categoryValues: string[];
     suggestions: string[];
