7d72cd1b54c3cf7b9c9f00900a46abfa1f5c9261
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 22:35:31 -0400

    remove recommendations

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import SaytAutocomplete from './autocomplete';
 import './categories';
 import './navigations';
 import SaytProducts from './products';
-import Recommendations from './recommendations';
 import './refinements';
 import Sayt from './sayt';
 import './search-terms';
 
-export { Sayt, SaytAutocomplete, SaytProducts, Recommendations };
+export { Sayt, SaytAutocomplete, SaytProducts };
