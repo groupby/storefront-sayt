960b72d737e9569221c98c52cba4078e9f9e5c8e
Filip Francetic <filip.francetic@groupbyinc.com>
2017-10-19 11:05:12 -0400

    Add past purchase tag

---
+++ src/past-purchase/index.ts
@@ -0,0 +1,37 @@
+import { alias, tag, Events, Selectors, Tag } from '@storefront/core';
+import Autocomplete from '../autocomplete';
+
+@alias('saytCategories')
+@tag('gb-sayt-past-purchase', require('./index.html'))
+class PastPurchase {
+
+  state: PastPurchase.State = {
+    // onClick: ({ matchAll, value }) => () =>
+    //   this.actions.updateSearch({
+    //     clear: true,
+    //     query: Selectors.autocompleteQuery(this.flux.store.getState()),
+    //     ...(<any>matchAll || {
+    //       navigationId: this.$autocomplete.category,
+    //       value
+    //     })
+    //   }),
+    query: Selectors.autocompleteQuery(this.flux.store.getState()),
+    pastPurchases: Selectors.queryPastPurchases(this.flux.store.getState())
+  };
+
+  init() {
+    this.flux.on(Events.AUTOCOMPLETE_QUERY_UPDATED, this.updateQuery);
+  }
+
+  updateQuery = (query: string) => this.set({ query });
+}
+
+interface PastPurchase extends Tag { }
+namespace PastPurchase {
+  export interface State {
+    query: string;
+    pastPurchases: any[];
+  }
+}
+
+export default PastPurchase;
