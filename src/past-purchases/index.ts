ca44fe471c428a691a919dff5dbdcc2ed979bc72
Filip Francetic <filip.francetic@groupbyinc.com>
2017-10-23 12:35:55 -0400

    Reduce size of past-purchases/index.ts

--- src/past-purchases/index.ts
+++ src/past-purchases/index.ts
@@ -1,17 +1,7 @@
-import { alias, tag, Tag } from '@storefront/core';
+import { tag } from '@storefront/core';
 
-@alias('past-purchases')
 @tag('gb-sayt-past-purchases', require('./index.html'))
 class PastPurchases {
-
-  state: PastPurchases.State = {
-  };
-}
-
-interface PastPurchases extends Tag { }
-namespace PastPurchases {
-    export interface State {
-    }
 }
 
 export default PastPurchases;
